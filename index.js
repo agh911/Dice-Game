ea3d7ee32a940d0250e7ebe572ab7bb96833d129
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2022-11-28 23:08:35 +0000

    updated code

---
+++ index.js
@@ -0,0 +1,24 @@
+// Player 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource = 'images/dice' + randomNumber1 + '.png';
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute('src', randomImageSource);
+
+// Player 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = 'images/dice' + randomNumber2 + '.png';
+
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute('src', randomImageSource2);
+
+// Match winning/loosing message
+if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = '🏆Player1 wins!'
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector('h1').innerHTML = ' Player2 wins!🏆'
+} else {
+    document.querySelector('h1').innerHTML = "It's a draw!"
+}
